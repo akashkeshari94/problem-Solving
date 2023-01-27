ed32ab835c02f6dcf4068925765b3c94d7846cc1
Akash Keshari <30547546+akashkeshari94@users.noreply.github.com>
2023-01-27 22:57:18 +0530

    Day3Of100DaysOfCoding

--- NestedSwitch.java
+++ NestedSwitch.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class NestedSwitch {
+import java.util.Scanner;
+
+public class NestedSwitch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int employeeId=sc.nextInt();
+        String department=sc.next();
+         switch (employeeId){
+             case 1:
+                 System.out.println("Akash Keshari");
+                 break;
+             case 2:
+                 System.out.println("Pratiksha Keshari");
+                 break;
+             case 3:
+                 switch (department){
+                     case "IT":
+                         System.out.println("IT Deptt");
+                         break;
+                     case "Management":
+                         System.out.println("Management Deptt");
+                         break;
+                     default:
+                         System.out.println("Enter valid Deptt");
+                 }
+                 break;
+             default:
+                 System.out.println("Enter valid Emp id");
+         }
+
+    }
 }
