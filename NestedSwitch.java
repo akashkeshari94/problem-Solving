6b13cc4c76a1c42230eb35c5d38a44af5e579164
Keshari, Akash <akash_keshari@optum.com>
2023-02-08 16:21:08 +0530

    Day4Of100DaysCoding

---
+++ NestedSwitch.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NestedSwitch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int employeeId=sc.nextInt();
+        String department=sc.next();
+
+        switch (employeeId){
+            case 1:
+                System.out.println("Akash Keshari");
+                break;
+
+            case 2:
+                System.out.println("Pratiksha Gupta");
+                break;
+
+            case 3:
+                switch (department){
+                    case "IT":
+                        System.out.println("IT Deptt");
+                        break;
+                    case "Managemnet":
+                        System.out.println("Management Deptt");
+                        break;
+                    default:
+                        System.out.println("No Department Entered");
+                }
+                break;
+            default:
+                System.out.println("Enter correct Employee ID");
+        }
+    }
+}
