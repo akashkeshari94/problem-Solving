8e1e66851619c37c789bd49abff92f06c6432ada
Keshari, Akash <akash_keshari@optum.com>
2023-02-10 02:10:07 +0530

    Day5Of100DaysOfCoding

--- Sum.java
+++ Sum.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Sum {
 
     public static void main(String[] args) {
-        sum();
+        //sum();
+        int ans=sum2();
+        System.out.println(ans);
     }
 
     private static void sum() {
@@ -14,6 +16,18 @@ public class Sum {
         int num2=sc.nextInt();
         int sum=num1+num2;
         System.out.println("The sum of "+num1+" and "+ num2+" is "+sum);
+
+    }
+
+    //return a value
+    private static int sum2() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int num1=sc.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2=sc.nextInt();
+        int sum=num1+num2;
+        return sum;
 
     }
 }
